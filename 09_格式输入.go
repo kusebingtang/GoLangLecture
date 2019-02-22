abc622471d7fcb5dcd543a4be36b690e5f98e87a
kusebingtang <kusebintang@163.com>
2019-02-22 20:06:54 +0800

    格式化输入输出

---
+++ 09_格式输入.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	var a int
+
+	//&是一个运算符，取地址运算符
+	fmt.Scan(&a)
+
+	//输出的内内存地址0xc0000160a0
+	fmt.Println(&a)
+
+	fmt.Println(a)
+
+	var a1,b1 int
+
+	//空格或者回车输入
+	fmt.Scan(&a1,&b1)
+
+	fmt.Println(a1)
+	fmt.Println(b1)
+}
