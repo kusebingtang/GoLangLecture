68f003286eb8b203ec51c6c261d7a0218515d8e2
kusebingtang <kusebintang@163.com>
2019-02-22 17:40:01 +0800

    多重赋值和匿名变量

---
+++ 06_多重赋值和匿名变量.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a, b := 10 ,20
+
+	var temp int
+
+	//交换两个变量的值
+	temp = a
+	a = b
+	b =  temp
+
+	fmt.Printf("a = %d ,b = %d \n", a, b)
+
+	//go 语言的多种赋值，可以直接省略中间变量，做两个变量的值的交换操作
+	var i, j  = 10 ,20
+	i,j = j,i
+	fmt.Printf("i = %d ,j = %d \n", i, j)
+
+	//_匿名变量，丢弃数据不处理
+	temp, _ = i, j
+	fmt.Println("temp = ",temp)
+}
