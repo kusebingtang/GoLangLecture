261a19d093a73383e910596529de85a32edc8c32
kusebingtang <kusebintang@163.com>
2019-06-13 17:31:35 +0800

    保存任务接口

--- go_crontab_lecture/master/ApiServer.go
+++ go_crontab_lecture/master/ApiServer.go
@@ -1,8 +1,12 @@
 package master
 
 import (
+	"GoLecture/go_crontab_lecture/common"
+	"encoding/json"
+	"fmt"
 	"net"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -11,19 +15,53 @@ type ApiServer struct {
 }
 
 var (
-	G_apiServer * ApiServer
+	G_apiServer *ApiServer
 )
 
 // 保存任务接口
 // POST job={"name": "job1", "command": "echo hello", "cronExpr": "* * * * *"}
 func handleJobSave(resp http.ResponseWriter, req *http.Request) {
 
+	fmt.Println("handleJobSave")
 
+	var (
+		err     error
+		postJob string
+		addJob  common.Job
+		bytes   []byte
+		oldJob  *common.Job
+	)
 
+	if err = req.ParseForm(); err != nil {
+		fmt.Println(err)
+		goto ERR
+	}
+
+	postJob = req.PostForm.Get("job")
+
+	//fmt.Println(postJob)
+
+	//json 反序列化addJob
+	if err = json.Unmarshal([]byte(postJob), &addJob); err != nil {
+		goto ERR
+	}
+
+	//fmt.Println(addJob)
+
+	if oldJob, err = G_jobMgr.SaveJob(&addJob); err != nil {
+		goto ERR
+	}
+	//返回成功应答
+	if bytes, err = common.BuildResponse(0, "success", oldJob); err == nil {
+		resp.Write(bytes)
+	}
+	return
+
+ERR:
+	fmt.Println(err)
 }
 
-
-func InitApiServer()(err error) {
+func InitApiServer() (err error) {
 	var (
 		mux        *http.ServeMux
 		listener   net.Listener
@@ -31,22 +69,22 @@ func InitApiServer()(err error) {
 	)
 
 	mux = http.NewServeMux()
-	mux.HandleFunc("/job/save",handleJobSave)
+	mux.HandleFunc("/job/save", handleJobSave)
 
 	//启动TCP监听
-	if listener,err = net.Listen("tcp",":8090"); err != nil {
+	if listener, err = net.Listen("tcp", ":"+strconv.Itoa(G_config.ApiPort)); err != nil {
 		return
 	}
 
 	httpServer = &http.Server{
-		ReadTimeout:5 * time.Second,
-		WriteTimeout:5 * time.Second,
-		Handler:mux,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		Handler:      mux,
 	}
 
 	//单例模式初始化
 	G_apiServer = &ApiServer{
-		httpServer:httpServer,
+		httpServer: httpServer,
 	}
 
 	go httpServer.Serve(listener)
--- go_crontab_lecture/master/JobMgr.go
+++ go_crontab_lecture/master/JobMgr.go
@@ -1,6 +1,9 @@
 package master
 
 import (
+	"GoLecture/go_crontab_lecture/common"
+	"context"
+	"encoding/json"
 	"go.etcd.io/etcd/clientv3"
 	"time"
 )
@@ -8,46 +11,75 @@ import (
 //任务管理器
 type JobMgr struct {
 	client *clientv3.Client
-	kv clientv3.KV
-	lease clientv3.Lease
+	kv     clientv3.KV
+	lease  clientv3.Lease
 }
 
 var (
 	G_jobMgr *JobMgr
 )
 
-func InitJobMgr()(err error) {
+func InitJobMgr() (err error) {
 
 	var (
 		config clientv3.Config
 		client *clientv3.Client
-		kv clientv3.KV
-		lease clientv3.Lease
+		kv     clientv3.KV
+		lease  clientv3.Lease
 	)
 
-
 	config = clientv3.Config{
-		Endpoints:G_config.EtcdEndpoints,  //集群地址
-		DialTimeout:time.Duration(G_config.EtcdDialTimeout)* time.Millisecond,//连接超时
+		Endpoints:   G_config.EtcdEndpoints,                                     //集群地址
+		DialTimeout: time.Duration(G_config.EtcdDialTimeout) * time.Millisecond, //连接超时
 
 	}
 
-	if client,err = clientv3.New(config); err !=nil {
+	if client, err = clientv3.New(config); err != nil {
 		return
 	}
 
 	kv = clientv3.NewKV(client)
 	lease = clientv3.NewLease(client)
 
-
 	G_jobMgr = &JobMgr{
 		client: client,
-		kv: kv,
-		lease:lease,
-
+		kv:     kv,
+		lease:  lease,
 	}
 
 	//fmt.Println(client)
 
+	return
+}
+
+// 保存任务
+func (jobMgr *JobMgr) SaveJob(job *common.Job) (oldJob *common.Job, err error) {
+	// 把任务保存到/cron/jobs/任务名 -> json
+
+	var (
+		jobKey        string
+		jobBytesValue []byte
+		putResp       *clientv3.PutResponse
+		oldJobObject  common.Job
+	)
+
+	jobKey = common.JOB_SAVE_DIR + job.Name
+
+	if jobBytesValue, err = json.Marshal(job); err != nil {
+		return
+	}
+
+	//保存到etcd
+	if putResp, err = jobMgr.kv.Put(context.TODO(), jobKey, string(jobBytesValue), clientv3.WithPrevKV()); err != nil {
+		return
+	}
+
+	//put success 返回久值
+	if putResp.PrevKv != nil {
+		if err = json.Unmarshal(putResp.PrevKv.Value, &oldJobObject); err != nil {
+			return
+		}
+		oldJob = &oldJobObject
+	}
 	return
 }
